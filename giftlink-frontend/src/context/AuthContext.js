b7b7b1d578ffbbaf39787b29f86fccf785b684a4
kanyora-n <kanyorawangu8424@gmail.com>
2025-02-27 23:53:37 +0300

    Update AuthContext.js

--- giftlink-frontend/src/context/AuthContext.js
+++ giftlink-frontend/src/context/AuthContext.js
@@ -5,6 +5,7 @@ const AppContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
   return (
     <AppContext.Provider value={{ isLoggedIn, setIsLoggedIn, userName, setUserName }}>
