e551700a7a09c4b23f8a81a9f8078067c7a875a9
kanyora-n <kanyorawangu8424@gmail.com>
2025-02-27 23:27:53 +0300

    Update LoginPage.js

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useAppContext from '../context/AuthContext';
+import useAppContext from '../../context/AuthContext';
 import { urlConfig } from '../../config';
 import './LoginPage.css';
 
